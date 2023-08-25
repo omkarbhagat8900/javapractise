d0f60a7cbad59bab05d114b10195a70b6b1f20ee
omkar bhagat <142728705+omkarbhagat8900@users.noreply.github.com>
2023-08-25 23:46:09 +0530

    Add files via upload

---
+++ practise.java
@@ -0,0 +1,36 @@
+//calculate perimeter and area of rectangle have length = 13 and breadth = 14.
+
+
+class practise{
+   /*  public static void main(String []args){
+        //Area = lenght * Breadth
+        int area = 13*14;
+        System.out.println(area);
+         
+        //Perimeter = 2*(length + Breadth)
+        int perimeter = 2*(13+14);
+        System.out.print(perimeter);
+
+
+    }
+
+}*/
+
+/*~~~~~~~~~~~~~~~~~~~ output ~~~~~~~~~~~~~~~~~~~~ 
+
+182
+54*/
+
+/*--------------------------------------------------------------------------------- */
+
+/*Write a program which does the following
+Output the result - Difference of the integers 9 & 2 
+Output the result - Division of 30 by 5
+Output both results to the console on separate lines*/
+
+    public static void main(String[] args) {
+    System.out.println(9-2);
+    System.out.println(30/5);
+}
+
+}
