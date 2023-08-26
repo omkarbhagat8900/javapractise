f856e0f7dcb5bbcbc92a26b1a32a5e010bb2de7b
omkar bhagat <142728705+omkarbhagat8900@users.noreply.github.com>
2023-08-26 23:51:01 +0530

    Add files via upload

---
+++ problem.java
@@ -0,0 +1,20 @@
+//Find the area of a square of side 14cm.
+//Also, find the cost of painting the square if the paint costs is $7 per 
+
+
+class area{
+    public static void main (String[]args){
+        int side = 14;
+        int area = 14*14;
+        int cost = area*7;
+
+        System.out.println("area is :" + area);
+        System.out.println("cost is :" + cost +"$");
+
+    }
+}
+
+/* ~~~~~~~~~~~ output ~~~~~~~~~~~~~~~~~
+area is :196
+cost is :1372$
+*/
