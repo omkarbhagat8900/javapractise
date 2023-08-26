365babb86217dd999417a18ab7193ebdf5eb8b49
omkar bhagat <142728705+omkarbhagat8900@users.noreply.github.com>
2023-08-26 23:30:46 +0530

    Add files via upload

---
+++ area.java
@@ -0,0 +1,19 @@
+//Find out and display the area of a rectangle of sides 45 and 76 respectively.
+
+
+class practise {
+     public static void main(String[] args) {
+        int Length = 45;
+        int Breadth = 76;
+        int Area = Length*Breadth;
+
+        System.out.print("Area is :"+ Area );
+
+
+    }
+}
+
+
+/*~~~~~~~~~~~~~~~ Output ~~~~~~~~~~~~~~~~~ 
+Area is :3420
+*/
